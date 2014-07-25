120d489f7e930f0dfd27de7649c053b2e576930c
ngyuki <ngyuki.ts@gmail.com>
2014-07-25 22:52:54 +0900

    add tests

---
+++ tests/Test/Controller/IndexControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace SampleTest\Controller;
+
+class SampleTest extends \Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase
+{
+    protected $traceError = true;
+
+    protected function setUp()
+    {
+        $this->setApplicationConfig(require APP_CONFIG);
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    function index_()
+    {
+        $this->dispatch('/');
+
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('index');
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+define('APP_CONFIG', __DIR__ . '/../example/config/application.config.php');
